fb2d0edbcb0689f8d8b2d2b04b23cbd48bf4359a
Alban14550 <alban.florentin@eleve.ensai.fr>
2025-02-25 18:26:53 +0100

    Update combinaison.py

--- DM_rami/combinaison.py
+++ DM_rami/combinaison.py
@@ -55,7 +55,7 @@ class Combinaison():
              # pour les comparer, il faut que les deux combinaison soient dans le même ordre
             #  sinon il y a des combinaisons qui vont être considérées comme non égales.
         return self.cartes == other.cartes
-        #return sorted(self.cartes) == sorted(other.cartes), il ne faut pas self.__cartes et orther.__cartes?, ce n'est pas privé 
+        #return sorted(self.cartes) == sorted(other.cartes), il ne faut pas self.__cartes et orther.__cartes?, c'est privé 
 
     def __repr__(self):
         cartes_repr = [carte.__repr__() for carte in self.__cartes]
@@ -64,11 +64,6 @@ class Combinaison():
     def __str__(self):
         cartes_str = [carte.__str__() for carte in self.__cartes]
         return "(" + ", ".join(cartes_str) + ")"
-        #la méthode spéciale __str__() de la classe Combinaison renvoie la représentation informelle d’une combinai-
-        #son, correspondant au tuple de la représentation informelle de chaque carte. Par exemple, la représentation in-
-        #formelle de Combinaison((Carte('As', 'Pique'), Carte('As', 'Carreau'), Carte('As', 'Trêfle')))
-        #est "(As de pique, As de carreau, As de trêfle)".
-        # j'aurai mis une virgule et non un + entre les deux.
 
     def __len__(self):
         return len(self.__cartes)
@@ -86,7 +81,7 @@ class Combinaison():
     def est_carree(self):
         if len(self) != 4:
             return False
-        valeurs = set([carte.valeur for carte in self.cartes])
+        valeurs = set([carte.valeur for carte in self.__cartes])
         couleurs = set([carte.couleur for carte in self.__cartes])
         if len(valeurs) == 1 and len(couleurs) == 4:
             return True
