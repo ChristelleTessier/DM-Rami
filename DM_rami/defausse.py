2c845127d4c28a14a52c9eefc531ed9d33397857
Alban14550 <alban.florentin@eleve.ensai.fr>
2025-02-25 18:56:17 +0100

    Update defausse.py

--- DM_rami/defausse.py
+++ DM_rami/defausse.py
@@ -8,7 +8,11 @@ from carte import Carte
 class Defausse(_ListeCartes):
     """ """
     def __init__(self,cartes):
-        self.__cartes = cartes
+        self.cartes = cartes
 
     def vider(self):
         return 1
+
+    def __str__(self):
+        """ Affichage de la défausse """
+        return f"Défausse: [{', '.join(str(carte) for carte in self.cartes)}]"
