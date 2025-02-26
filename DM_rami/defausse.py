01aac5a404fd7db43f6eebba40d98b13e980f9fb
Alban14550 <alban.florentin@eleve.ensai.fr>
2025-02-26 15:52:13 +0100

    Update defausse.py

--- DM_rami/defausse.py
+++ DM_rami/defausse.py
@@ -1,20 +1,37 @@
 """Implémentation de la classe Defausse."""
 
 
+import random
 from base import _ListeCartes
 from carte import Carte
 
-
 class Defausse(_ListeCartes):
-    """ """
-    def __init__(self, cartes = []):
-        """ Constructeur """
-        super().__init__(cartes) # Utiliser le constructeur de la classe mère (_ListeCartes)
-        self.__cartes = cartes
+    """Classe Defausse modélisant la liste des cartes jetées."""
 
-    def vider(self):
-        return 1
+    def __init__(self, cartes=None):
+        """
+        Initialise la défausse en appelant le constructeur de _ListeCartes.
+        :param cartes: Liste de cartes initiales (par défaut vide).
+        """
+        if cartes is None:
+            cartes = []  # Éviter l'utilisation d'une liste mutable comme valeur par défaut
+        super().__init__(cartes)  # Utilisation correcte du constructeur parent
+
+    def vider(self, reserve):
+        """
+        Mélange la défausse et l'ajoute à la fin de la réserve.
+        :param reserve: Instance de la classe Réserve.
+        """
+        if not self._cartes:  # Vérifie si la défausse est vide
+            return False
+        
+        random.shuffle(self._cartes)  # Mélange les cartes
+        reserve._cartes.extend(self._cartes)  # Ajoute les cartes mélangées à la réserve
+        self._cartes.clear()  # Vide la défausse
+        return True  # Indique que l'opération a réussi
 
     def __str__(self):
-        """ Affichage de la défausse """
-        return f"Défausse: [{', '.join(str(carte) for carte in self.cartes)}]"
+        """Affichage de la défausse sous forme de chaîne."""
+        return f"Défausse: [{', '.join(str(carte) for carte in self._cartes)}]"
+
+  
