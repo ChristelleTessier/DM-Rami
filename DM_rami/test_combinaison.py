e8f0e153aa0dd778b748a67bea19bc2e44e4b067
Alban14550 <alban.florentin@eleve.ensai.fr>
2025-02-28 09:06:00 +0100

    Update test_combinaison.py

--- DM_rami/test_combinaison.py
+++ DM_rami/test_combinaison.py
@@ -1,5 +1,4 @@
 """Implémentation des tests pour la classe Combinaison."""
-"""Implémentation des tests pour la classe Combinaison."""
 import pytest
 from carte import Carte
 from combinaison import Combinaison
@@ -7,15 +6,12 @@ from combinaison import Combinaison
 
 def test_init():
     """Test du constructeur de Combinaison."""
-    #GIVEN
     c1 = Carte("As", "Pique")
     c2 = Carte("Roi", "Pique")
     c3 = Carte("Dame", "Pique")
 
-    #WHEN
     combi = Combinaison((c1, c2, c3))
 
-    #THEN
     assert isinstance(combi, Combinaison)
     assert len(combi) == 3
 
@@ -28,19 +24,17 @@ def test_init():
 
 def test_eq():
     """Test de l'égalité entre combinaisons."""
-    #GIVEN
     c1 = Carte("As", "Pique")
     c2 = Carte("Roi", "Pique")
     c3 = Carte("Dame", "Pique")
+    
 
-    #WHEN
     combi1 = Combinaison((c1, c2, c3))
     combi2 = Combinaison((c1, c2, c3))
     combi3 = Combinaison((c3, c2, c1))  # Ordre différent
 
-    #THEN
     assert combi1 == combi2
-    assert combi1 == combi3
+    assert combi1 != combi3
     assert combi1 != "string"  # Comparaison avec un autre type
 
 
@@ -51,17 +45,20 @@ def test_est_brelan():
     c2 = Carte("As", "Coeur")
     c3 = Carte("As", "Carreau")
     c4 = Carte("Roi", "Pique")
-
+    c5 = Carte("As", "Pique")
     # WHEN
     combi_brelan = Combinaison((c1, c2, c3))
     combi_pas_brelan = Combinaison((c1, c2, c4))
-
+    combi_pas_brelan2 = Combinaison((c1, c2, c5))
+    
     #THEN
     assert combi_brelan.est_brelan() is True
     assert combi_pas_brelan.est_brelan() is False
+    assert combi_pas_brelan2.est_brelan() is False
 
 
 def test_est_carre():
+        
     """Test de la détection d'un carré."""
     # GIVEN
     c1 = Carte("As", "Pique")
@@ -69,16 +66,18 @@ def test_est_carre():
     c3 = Carte("As", "Carreau")
     c4 = Carte("As", "Trêfle")
     c5 = Carte("Roi", "Pique")
+    c6 = Carte("As", "Trêfle")
 
      # WHEN
-    combi_carre = Combinaison((c1, c2, c3, c4))
+    combi_carre = Combinaison((c1, c2, c3, c4))   
     combi_pas_carre = Combinaison((c1, c2, c3, c5))
-
+    combi_pas_carre2 = Combinaison((c1, c2, c4, c6))
+    
     #THEN
     assert combi_carre.est_carre() is True
     assert combi_pas_carre.est_carre() is False
-
-
+    assert combi_pas_carre2.est_carre() is False
+    
 def test_est_sequence():
     """Test de la détection d'une séquence."""
     #GIVEN
@@ -88,9 +87,9 @@ def test_est_sequence():
     c4 = Carte("As", "Coeur")
 
     #WHEN
-    combi_sequence = Combinaison((c1, c2, c3))
+    combi_sequence = Combinaison((c1, c2, c3)) 
     combi_pas_sequence = Combinaison((c1, c2, c4))  # Couleurs différentes
-
+    
     #THEN
     assert combi_pas_sequence.est_sequence() is False
     assert combi_sequence.est_sequence() is True
@@ -109,9 +108,9 @@ def test_est_valide():
     combi_brelan = Combinaison((c1, c2, c3))
     combi_carre = Combinaison((c1, c2, c3, c4))
     combi_invalide = Combinaison((c1, c2, c5))
-
+    
     #THEN
-    assert combi_brelan.est_valide() is True
+    assert combi_brelan.est_valide() is True  
     assert combi_carre.est_valide() is True
     assert combi_invalide.est_valide() is False
 
@@ -127,7 +126,7 @@ def test_calcule_nombre_points():
     #WHEN
     combi_brelan = Combinaison((c1, c2, c3))
     combi_carre = Combinaison((c1, c2, c3, c4))
-
+    
     #THEN
     assert combi_brelan.calcule_nombre_points() == 33  # Chaque As vaut 11
     assert combi_carre.calcule_nombre_points() == 44 # Chaque As vaut 11
